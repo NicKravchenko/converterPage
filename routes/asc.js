43db95ccb9412461bcc6139dfa771f33f883f111
Nikita Kravchenko <89710564+NicKravchenko@users.noreply.github.com>
2022-11-14 22:41:06 -0400

    almost working

--- routes/asc.js
+++ routes/asc.js
@@ -6,7 +6,7 @@ router.get('/', (req, res, next) => {
     const mode = req.body.mode ? req.body.mode : "charToAsc";
     const asc = 3;
     const char = 'A';
-
+    console.log("Im in main");
     res.render('Pages/asc', {
         title : 'Asc convertor',
         mode: mode,
@@ -18,11 +18,31 @@ router.get('/', (req, res, next) => {
 router.post('/changeMode', (req, res, next) => {
     const mode = req.body.mode;
 
+    console.log("Im in change mod");
+
+
     res.render("Pages/asc",{
         title : 'Asc convertor',
-        mode: mode
+        mode: mode,
+        asc : "",
+        char : ""
         });
 });
+
+router.post('/ascToChar', (req, res, next) => {
+    const mode = req.body.mode;
+    console.log ( mode);
+    const asc = req.body.asc;
+    const char = String.fromCharCode(asc);
+
+    res.render("Pages/asc",{
+        title : 'Asc convertor',
+        mode: mode,
+        asc : asc,
+        char : char
+        });
+
+})
 
 
 module.exports = router;
