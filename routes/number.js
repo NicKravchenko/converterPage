77f5e03396cd719719e8791145a52e611ffa818f
Nikita Kravchenko <89710564+NicKravchenko@users.noreply.github.com>
2022-11-17 21:56:32 -0400

    Done

--- routes/number.js
+++ routes/number.js
@@ -4,8 +4,22 @@ const router = express.Router();
 
 router.get('/', (req, res, next) => {
     res.render('Pages/number', {
-        title: 'Number convertor'
+        title: 'Number convertor',
+        decimalNum: '',
+        binaryNum: ''
     });
 });
+
+router.post('/', (req, res, next) => {
+    const dn = req.body.decimalNum;
+
+    bn = Number(dn).toString(2);
+    
+    res.render('Pages/number', {
+        title: 'Number convertor',
+        decimalNum: dn,
+        binaryNum: bn
+    });
+})
 
 module.exports = router;
